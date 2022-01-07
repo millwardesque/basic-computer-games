eea14dd5a14dcfb0d0f603d45f5477b85638288a
christopher.millward <christopher.millward@cloudkitchens.com>
2022-01-07 17:07:07 -0500

    Variable renaming and magic variable replacement

--- 02_Amazing/javascript/amazing.js
+++ 02_Amazing/javascript/amazing.js
@@ -5,6 +5,7 @@
 
 /**
  * @TODO
+ * Replace method for deciding direction with a much smarter approach of choosing one element from an array of valid directions
  * Variable rename
  * Refactor into functions
  * 0-based indexing
@@ -12,6 +13,11 @@
  * Remove @DEBUG
  * Explore real refactoring
  */
+
+const LEFT = 1;
+const UP = 2;
+const RIGHT = 3;
+const DOWN = 4;
 
 function print(str) {
     document.getElementById("output").appendChild(document.createTextNode(str));
@@ -28,7 +34,7 @@ function input(defaultValue) {
         document.getElementById("output").appendChild(input_element);
         input_element.focus();
         input_element.addEventListener("keydown", function (event) {
-            if (event.keyCode == 13) {
+            if (event.keyCode === 13) {
                 const input_str = input_element.value;
                 document.getElementById("output").removeChild(input_element);
                 print(input_str);
@@ -86,7 +92,7 @@ function initGrids(rows, columns) {
 }
 
 function isExplorationComplete(rows, columns, nextStepCounter) {
-    return nextStepCounter == columns * rows + 1;
+    return nextStepCounter === columns * rows + 1;
 }
 
 function printMaze(rows, columns, cellWalls) {
@@ -104,7 +110,7 @@ function printMaze(rows, columns, cellWalls) {
         // Print the row's lower walls
         str = "";
         for (let i = 1; i <= columns; i++) {
-            if (cellWalls[i][j] == 0 || cellWalls[i][j] == 2)
+            if (cellWalls[i][j] === 0 || cellWalls[i][j] === 2)
                 str += ":--";
             else
                 str += ":  ";
@@ -115,7 +121,7 @@ function printMaze(rows, columns, cellWalls) {
 
 function printVisitHistory(rows, columns, stepHistory) {
     for (let j = 1; j <= rows; j++) {
-        let str = "I";
+        let str = "I ";
         for (let i = 1; i <= columns; i++) {
             str += stepHistory[i][j] + " ";
         }
@@ -136,36 +142,38 @@ async function main() {
     const [stepHistory, v] = initGrids(rows, columns);
     // stepHistory is a 2d grid of numbers which indicate the step on which I already visited each cell (or 0 for unvisited)
     // 'v' is a 2d grid of numbers which identify what walls each cell has
-    // v == 0: Right wall, bottom wall
-    // v == 1: Right wall
-    // v == 2: Bottom wall
+    // v === 0: Right wall, bottom wall
+    // v === 1: Right wall
+    // v === 2: Bottom wall
 
     // Choose a starting cell
-    let nextColumn = Math.floor(Math.random() * columns + 1);
+    let startColumn = Math.floor(Math.random() * columns + 1);
     let q = 0;
     let z = 0;
 
     // Print the top row's vertical walls
-    // X represents the starting cell, so it's blank
+    // startColumn represents the starting cell, so it's blank
     // All other cells in this direction are blocked
     for (i = 1; i <= columns; i++) {
-        if (i == nextColumn)
+        if (i === startColumn)
             print(".  ");
         else
             print(".--");
     }
     print(".\n");
 
-
-    let nextStepCounter = 1;  // Next-step order. Its current value is the 'step' number that the next cell we explore will have.
-    stepHistory[nextColumn][1] = nextStepCounter;
+    let nextStepCounter = 1;  // Represents the 'step' number that the next cell we explore will have.
+    stepHistory[startColumn][1] = nextStepCounter;
     nextStepCounter++;
-    let currentColumn = nextColumn;
+    let currentColumn = startColumn;
     let currentRow = 1;
     let entry = 0;  // This *could* be an indicator for where I can go next
-    let x = nextColumn; // @DEBUG Used for backwards compatibility because there's a lot of 'x' references that need to be updated to nextColumn
     while (1) {
-        if (entry == 2) {	// Search for a non-explored cell
+        let nextDirection = 4;
+
+        // We can't reach anywhere new from our current square,
+        // so pick a different square we've already explored and start from there.
+        if (entry === 2) {	// Search for a non-explored cell
             do {
                 if (currentColumn < columns) {
                     currentColumn++;
@@ -176,123 +184,152 @@ async function main() {
                     currentColumn = 1;
                     currentRow = 1;
                 }
-            } while (stepHistory[currentColumn][currentRow] == 0) ;
+            } while (stepHistory[currentColumn][currentRow] === 0) ;
         }
-        if (entry == 0 && currentColumn - 1 > 0 && stepHistory[currentColumn - 1][currentRow] == 0) {	// Can go left?
-            if (currentRow - 1 > 0 && stepHistory[currentColumn][currentRow - 1] == 0) {	// Can go up?
-                if (currentColumn < columns && stepHistory[currentColumn + 1][currentRow] == 0) {	// Can go right?
+
+        // Decide where to go next.
+        if (entry === 0 && currentColumn - 1 > 0 && stepHistory[currentColumn - 1][currentRow] === 0) {	// Can go left?
+            if (currentRow - 1 > 0 && stepHistory[currentColumn][currentRow - 1] === 0) {	// Can go up?
+                if (currentColumn < columns && stepHistory[currentColumn + 1][currentRow] === 0) {	// Can go right?
                     // Choose left/up/right
-                    x = Math.floor(Math.random() * 3 + 1);
+                    nextDirection = Math.floor(Math.random() * 3 + 1);
                 } else if (currentRow < rows) {
-                    if (stepHistory[currentColumn][currentRow + 1] == 0) {	// Can go down?
+                    if (stepHistory[currentColumn][currentRow + 1] === 0) {	// Can go down?
                         // Choose left/up/down
-                        x = Math.floor(Math.random() * 3 + 1);
-                        if (x == 3)
-                            x = 4;
+                        nextDirection = Math.floor(Math.random() * 3 + 1);
+
+                        // Since right isn't a valid option but this randomization approach doesn't let us skip steps,
+                        // we'll treat the value of RIGHT as DOWN
+                        if (nextDirection === RIGHT)
+                            nextDirection = DOWN;
                     } else {
-                        x = Math.floor(Math.random() * 2 + 1);
+                        nextDirection = Math.floor(Math.random() * 2 + 1);
                     }
-                } else if (z == 1) {
-                    x = Math.floor(Math.random() * 2 + 1);
+                } else if (z === 1) {
+                    nextDirection = Math.floor(Math.random() * 2 + 1);
                 } else {
                     q = 1;
-                    x = Math.floor(Math.random() * 3 + 1);
-                    if (x == 3)
-                        x = 4;
+                    nextDirection = Math.floor(Math.random() * 3 + 1);
+
+                    // Since right isn't a valid option but this randomization approach doesn't let us skip steps,
+                    // we'll treat the value of RIGHT as DOWN
+                    if (nextDirection === RIGHT)
+                        nextDirection = DOWN;
                 }
-            } else if (currentColumn < columns && stepHistory[currentColumn + 1][currentRow] == 0) {	// Can go right?
+            } else if (currentColumn < columns && stepHistory[currentColumn + 1][currentRow] === 0) {	// Can go right?
                 if (currentRow < rows) {
-                    if (stepHistory[currentColumn][currentRow + 1] == 0) {	// Can go down?
+                    if (stepHistory[currentColumn][currentRow + 1] === 0) {	// Can go down?
                         // Choose left/right/down
-                        x = Math.floor(Math.random() * 3 + 1);
+                        nextDirection = Math.floor(Math.random() * 3 + 1);
                     } else {
-                        x = Math.floor(Math.random() * 2 + 1);
+                        nextDirection = Math.floor(Math.random() * 2 + 1);
                     }
-                    if (x >= 2)
-                        x++;
-                } else if (z == 1) {
-                    x = Math.floor(Math.random() * 2 + 1);
-                    if (x >= 2)
-                        x++;
+
+                    // Since UP isn't a valid option but this randomization approach doesn't let us skip steps,
+                    // we'll treat the value of UP as RIGHT and RIGHT as DOWN
+                    if (nextDirection >= UP)
+                        nextDirection++;
+                } else if (z === 1) {
+                    nextDirection = Math.floor(Math.random() * 2 + 1);
+
+                    // Since UP and DOWN aren't valid options but this randomization approach doesn't let us skip steps,
+                    // we'll treat the value of UP as RIGHT
+                    if (nextDirection >= UP)
+                        nextDirection++;
                 } else {
                     q = 1;
-                    x = Math.floor(Math.random() * 3 + 1);
-                    if (x >= 2)
-                        x++;
+                    nextDirection = Math.floor(Math.random() * 3 + 1);
+                    // Since UP isn't a valid option but this randomization approach doesn't let us skip steps,
+                    // we'll treat the value of UP as RIGHT and RIGHT as DOWN
+                    if (nextDirection >= UP)
+                        nextDirection++;
                 }
             } else if (currentRow < rows) {
-                if (stepHistory[currentColumn][currentRow + 1] == 0) {	// Can go down?
+                if (stepHistory[currentColumn][currentRow + 1] === 0) {	// Can go down?
                     // Choose left/down
-                    x = Math.floor(Math.random() * 2 + 1);
-                    if (x == 2)
-                        x = 4;
+                    nextDirection = Math.floor(Math.random() * 2 + 1);
+
+                    // Since UP and RIGHT aren't valid options but this randomization approach doesn't let us skip steps,
+                    // we'll treat the value of UP as DOWN
+                    if (nextDirection === UP)
+                        nextDirection = DOWN;
                 } else {
-                    x = 1;
+                    nextDirection = LEFT;
                 }
-            } else if (z == 1) {
-                x = 1;
+            } else if (z === 1) {
+                nextDirection = LEFT;
             } else {
                 q = 1;
-                x = Math.floor(Math.random() * 2 + 1);
-                if (x == 2)
-                    x = 4;
+                nextDirection = Math.floor(Math.random() * 2 + 1);
+
+                // Since UP and RIGHT aren't valid options but this randomization approach doesn't let us skip steps,
+                // we'll treat the value of UP as DOWN
+                if (nextDirection === UP)
+                    nextDirection = DOWN;
             }
-        } else if (currentRow - 1 > 0 && stepHistory[currentColumn][currentRow - 1] == 0) {	// Can go up?
-            if (currentColumn < columns && stepHistory[currentColumn + 1][currentRow] == 0) {
+        } else if (currentRow - 1 > 0 && stepHistory[currentColumn][currentRow - 1] === 0) {	// Can go up?
+            if (currentColumn < columns && stepHistory[currentColumn + 1][currentRow] === 0) {
                 if (currentRow < rows) {
-                    if (stepHistory[currentColumn][currentRow + 1] == 0)
-                        x = Math.floor(Math.random() * 3 + 2);
+                    if (stepHistory[currentColumn][currentRow + 1] === 0)
+                        nextDirection = Math.floor(Math.random() * 3 + 2);
                     else
-                        x = Math.floor(Math.random() * 2 + 2);
-                } else if (z == 1) {
-                    x = Math.floor(Math.random() * 2 + 2);
+                        nextDirection = Math.floor(Math.random() * 2 + 2);
+                } else if (z === 1) {
+                    nextDirection = Math.floor(Math.random() * 2 + 2);
                 } else {
                     q = 1;
-                    x = Math.floor(Math.random() * 3 + 2);
+                    nextDirection = Math.floor(Math.random() * 3 + 2);
                 }
             } else if (currentRow < rows) {
-                if (stepHistory[currentColumn][currentRow + 1] == 0) {
-                    x = Math.floor(Math.random() * 2 + 2);
-                    if (x == 3)
-                        x = 4;
+                if (stepHistory[currentColumn][currentRow + 1] === 0) {
+                    nextDirection = Math.floor(Math.random() * 2 + 2);
+
+                    // Since RIGHT isn't valid options but this randomization approach doesn't let us skip steps,
+                    // we'll treat the value of RIGHT as DOWN
+                    if (nextDirection === RIGHT)
+                        nextDirection = DOWN;
                 } else {
-                    x = 2;
+                    nextDirection = UP;
                 }
-            } else if (z == 1) {
-                x = 2;
+            } else if (z === 1) {
+                nextDirection = UP;
             } else {
                 q = 1;
-                x = Math.floor(Math.random() * 2 + 2);
-                if (x == 3)
-                    x = 4;
+                nextDirection = Math.floor(Math.random() * 2 + 2);
+                // Since RIGHT isn't valid options but this randomization approach doesn't let us skip steps,
+                // we'll treat the value of RIGHT as DOWN
+                if (nextDirection === RIGHT)
+                    nextDirection = DOWN;
             }
-        } else if (currentColumn < columns && stepHistory[currentColumn + 1][currentRow] == 0) {	// Can go right?
+        } else if (currentColumn < columns && stepHistory[currentColumn + 1][currentRow] === 0) {	// Can go right?
             if (currentRow < rows) {
-                if (stepHistory[currentColumn][currentRow + 1] == 0)
-                    x = Math.floor(Math.random() * 2 + 3);
+                if (stepHistory[currentColumn][currentRow + 1] === 0)
+                    nextDirection = Math.floor(Math.random() * 2 + 3);
                 else
-                    x = 3;
-            } else if (z == 1) {
-                x = 3;
+                    nextDirection = RIGHT;
+            } else if (z === 1) {
+                nextDirection = RIGHT;
             } else {
                 q = 1;
-                x = Math.floor(Math.random() * 2 + 3);
+                nextDirection = Math.floor(Math.random() * 2 + 3);
             }
         } else if (currentRow < rows) {
-            if (stepHistory[currentColumn][currentRow + 1] == 0) 	// Can go down?
-                x = 4;
+            if (stepHistory[currentColumn][currentRow + 1] === 0) 	// Can go down?
+                nextDirection = DOWN;
             else {
                 entry = 2;	// Blocked!
                 continue;
             }
-        } else if (z == 1) {
+        } else if (z === 1) {
             entry = 2;	// Blocked!
             continue;
         } else {
             q = 1;
-            x = 4;
+            nextDirection = DOWN;
         }
-        if (x == 1) {	// Left
+
+        // Move to the next cell.
+        if (nextDirection === LEFT) {
             stepHistory[currentColumn - 1][currentRow] = nextStepCounter;
             nextStepCounter++;
             v[currentColumn - 1][currentRow] = 2;
@@ -302,7 +339,7 @@ async function main() {
             }
             q = 0;
             entry = 0;
-        } else if (x == 2) {	// Up
+        } else if (nextDirection === UP) {
             stepHistory[currentColumn][currentRow - 1] = nextStepCounter;
             nextStepCounter++;
             v[currentColumn][currentRow - 1] = 1;
@@ -312,10 +349,10 @@ async function main() {
             }
             q = 0;
             entry = 0;
-        } else if (x == 3) {	// Right
+        } else if (nextDirection === RIGHT) {
             stepHistory[currentColumn + 1][currentRow] = nextStepCounter;
             nextStepCounter++;
-            if (v[currentColumn][currentRow] == 0)
+            if (v[currentColumn][currentRow] === 0)
                 v[currentColumn][currentRow] = 2;
             else
                 v[currentColumn][currentRow] = 3;
@@ -324,11 +361,11 @@ async function main() {
                 break;
             }
             entry = 1;
-        } else if (x == 4) {	// Down
-            if (q != 1) {	// Only if not blocked
+        } else if (nextDirection === DOWN) {	// Down
+            if (q !== 1) {	// Only if not blocked
                 stepHistory[currentColumn][currentRow + 1] = nextStepCounter;
                 nextStepCounter++;
-                if (v[currentColumn][currentRow] == 0)
+                if (v[currentColumn][currentRow] === 0)
                     v[currentColumn][currentRow] = 1;
                 else
                     v[currentColumn][currentRow] = 3;
@@ -339,12 +376,12 @@ async function main() {
                 entry = 0;
             } else {
                 z = 1;
-                if (v[currentColumn][currentRow] == 0) {
+                if (v[currentColumn][currentRow] === 0) {
                     v[currentColumn][currentRow] = 1;
                     q = 0;
                     currentColumn = 1;
                     currentRow = 1;
-                    while (stepHistory[currentColumn][currentRow] == 0) {
+                    while (stepHistory[currentColumn][currentRow] === 0) {
                         if (currentColumn < columns) {
                             currentColumn++;
                         } else if (currentRow < rows) {
@@ -368,6 +405,7 @@ async function main() {
     // Output
     printMaze(rows, columns, v);
 
+    print("\nVisit history\n");
     printVisitHistory(rows, columns, stepHistory);
 }
 
